6892e3c27021a92e9f26acb2c2699089e184a10f
Johonas@gmail.com <Johonas@gmail.com@02abe300-ba99-61c0-0e28-91ae4f56c1f8>
2012-02-17 11:46:20 +0000

    Project started

---
+++ trunk/DKGG-2012-Group22-iMat/src/dkgg2012group22imat/DKGG2012Group22iMatApp.java
@@ -0,0 +1,44 @@
+/*
+ * DKGG2012Group22iMatApp.java
+ */
+
+package dkgg2012group22imat;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class DKGG2012Group22iMatApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new DKGG2012Group22iMatView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of DKGG2012Group22iMatApp
+     */
+    public static DKGG2012Group22iMatApp getApplication() {
+        return Application.getInstance(DKGG2012Group22iMatApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(DKGG2012Group22iMatApp.class, args);
+    }
+}
