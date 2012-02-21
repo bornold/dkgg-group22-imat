d5536ef7c98cd6ed2a7cb7867cb24bfcd80ce5b7
Johonas@gmail.com <Johonas@gmail.com@02abe300-ba99-61c0-0e28-91ae4f56c1f8>
2012-02-21 11:33:20 +0000

    Work in progress on HistoryCarts

--- trunk/DKGG-2012-Group22-iMat/src/dkgg2012group22imat/controller/entrance/HistoryCartController.java
+++ trunk/DKGG-2012-Group22-iMat/src/dkgg2012group22imat/controller/entrance/HistoryCartController.java
@@ -4,10 +4,45 @@
  */
 package dkgg2012group22imat.controller.entrance;
 
+import dkgg2012group22imat.model.Model;
+import dkgg2012group22imat.view.entrance.HistoryCartPanel;
+import java.util.List;
+import se.chalmers.ait.dat215.project.Order;
+import se.chalmers.ait.dat215.project.ShoppingItem;
+
 /**
  *
  * @author jonas
  */
 public class HistoryCartController {
-    
+
+    private HistoryCartPanel view;
+    private Order order;
+    private List<ShoppingItem> items;
+    Model m = Model.getInstance();
+
+    public HistoryCartController(Order o, HistoryCartPanel historyCartPanel) {
+        this.order = o;
+        this.view = historyCartPanel;
+        view.dateLabel.setText(order.getDate().toString());
+        items = o.getItems();
+        double totPrice = 0;
+        for (ShoppingItem si : items) {
+            totPrice += si.getAmount() + si.getProduct().getPrice();
+        }
+        view.informationLabel.setText(items.size() + "varor för totalt " + totPrice);
+        int amount = items.size() > 4 ? 4 : items.size();
+        switch (amount) {
+            case 4:
+                view.icon4.setIcon(m.getImageIcon(items.get(3).getProduct(), 5, 5));
+            case 3:
+                view.icon3.setIcon(m.getImageIcon(items.get(2).getProduct(), 5, 5));
+            case 2:
+                view.icon2.setIcon(m.getImageIcon(items.get(1).getProduct(), 5, 5));
+            case 1:
+                view.icon1.setIcon(m.getImageIcon(items.get(0).getProduct(), 5, 5));
+                break;
+        }
+    }
 }
+
