102413ec610f1b6f330d48890ae3ca3aaeac326b
Johonas@gmail.com <Johonas@gmail.com@02abe300-ba99-61c0-0e28-91ae4f56c1f8>
2012-02-21 16:32:06 +0000

    Started implemented SavedCart and FavoriteCarts

--- trunk/DKGG-2012-Group22-iMat/src/dkgg2012group22imat/model/SavedCart.java
+++ trunk/DKGG-2012-Group22-iMat/src/dkgg2012group22imat/model/SavedCart.java
@@ -6,6 +6,7 @@ package dkgg2012group22imat.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import se.chalmers.ait.dat215.project.Product;
 import se.chalmers.ait.dat215.project.ShoppingItem;
 
 /**
@@ -13,17 +14,48 @@ import se.chalmers.ait.dat215.project.ShoppingItem;
  * @author jonas
  */
 public class SavedCart {
-   private List<ShoppingItem> cart = new ArrayList<ShoppingItem>();
+   private List<ShoppingItem> items = new ArrayList<ShoppingItem>();
    private String name;
-   private double totalPrice;
    public SavedCart(List<ShoppingItem> cart, String name) {
-       this.cart = cart;
+       this.items = cart;
        this.name = name;
    }
       public SavedCart( String name) {
        this.name = name;
    }
       public void add(ShoppingItem si) {
-          cart.add(si);
+          items.add(si);
       }
+      public void addAll(List<ShoppingItem> si) {
+          items.addAll(si);
+      }
+      public void remove(ShoppingItem si){
+          items.remove(si);
+      }
+      public void remove(Product p){
+          for (ShoppingItem si : items) {
+              if (si.getProduct().equals(p)){
+                  items.remove(si);
+              }
+          }
+      }
+      public List<ShoppingItem> getItems() {
+          return items;
+      }
+      public double getTotalPrice() {
+          double tot = 0;
+          for(ShoppingItem si : items) {
+              tot += si.getProduct().getPrice() * si.getAmount();
+          }
+          return tot;
+      }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+         
 }
