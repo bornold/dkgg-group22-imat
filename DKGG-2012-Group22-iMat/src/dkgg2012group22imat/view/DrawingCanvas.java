0e1b9ed29c9a41bd8424e4d400e2380cd082768a
sim.nilsson@gmail.com <sim.nilsson@gmail.com@02abe300-ba99-61c0-0e28-91ae4f56c1f8>
2012-02-26 11:52:04 +0000

    Better animation ^^

---
+++ DKGG-2012-Group22-iMat/src/dkgg2012group22imat/view/DrawingCanvas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dkgg2012group22imat.view;
+
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+/**
+ * @author Aurelien Ribon | http://www.aurelienribon.com
+ */
+public abstract class DrawingCanvas extends JPanel {
+        private Thread thread = null;
+        private boolean isRunning = false;
+
+        public DrawingCanvas start() {
+                thread = new Thread(loop);
+                thread.start();
+                isRunning = true;
+                return this;
+        }
+
+        public void stop() {
+                isRunning = false;
+                try {
+                        thread.join();
+                } catch (InterruptedException ex) {
+                }
+        }
+
+        protected abstract void update(int elapsedMillis);
+
+        private Runnable loop = new Runnable() {
+                private long lastMillis;
+
+                @Override public void run() {
+                        lastMillis = System.currentTimeMillis();
+
+                        while (isRunning) {
+                                try {
+                                        final long millis = System.currentTimeMillis();
+                                        final long delta = millis - lastMillis;
+                                        lastMillis = millis;
+
+                                        SwingUtilities.invokeAndWait(new Runnable() {@Override public void run() {update((int) delta);}});
+                                        Thread.sleep(16);
+
+                                } catch (InterruptedException ex) {
+                                } catch (InvocationTargetException ex) {
+                                }
+                        }
+                }
+        };
+}
