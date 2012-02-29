1c9e8704a3fab61435e9e9e826093fbed4854d1f
Johonas@gmail.com <Johonas@gmail.com@02abe300-ba99-61c0-0e28-91ae4f56c1f8>
2012-02-29 15:58:57 +0000

    Pagenation disable icon

--- DKGG-2012-Group22-iMat/src/dkgg2012group22imat/view/PagenationButton.java
+++ DKGG-2012-Group22-iMat/src/dkgg2012group22imat/view/PagenationButton.java
@@ -8,6 +8,8 @@ import dkgg2012group22imat.controller.ProductsPanelController;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /**
@@ -15,20 +17,23 @@ import javax.swing.JButton;
  * @author jonas
  */
 public class PagenationButton extends JButton implements ActionListener {
+
     private int page;
     private ProductsPanelController parent;
-    public PagenationButton(int aPage, ProductsPanelController aParent){
-        super(""+(aPage+1));
+
+    public PagenationButton(int aPage, ProductsPanelController aParent) {
+        super("" + (aPage + 1));
         this.page = aPage;
         this.parent = aParent;
+        Icon img = new ImageIcon(this.getClass().getResource("/dkgg2012group22imat/resources/plupp.png"));
+        setDisabledIcon(img);
+        setHorizontalTextPosition(CENTER);
         this.setBorderPainted(false);
-        this.setPreferredSize(new Dimension(60,60));
+        this.setPreferredSize(new Dimension(60, 60));
         super.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent ae) {
         parent.goToPage(page);
     }
-    
-    
 }
